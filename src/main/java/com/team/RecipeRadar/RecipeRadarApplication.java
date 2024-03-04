a65ce6e8c4a62f97fc6ea0c0a9bb766da8c39379
ggambopce <ggambopce@gmail.com>
2024-03-04 23:34:03 +0900

    Start

---
+++ src/main/java/com/team/RecipeRadar/RecipeRadarApplication.java
@@ -0,0 +1,13 @@
+package com.team.RecipeRadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/RecipeRadar/RecipeRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.RecipeRadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
