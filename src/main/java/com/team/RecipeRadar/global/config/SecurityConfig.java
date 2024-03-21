fb7203df7fe8fa49aa0f5045c4d6824a1b9b079e
CHISANW <keuye06380618@gmail.com>
2024-03-22 02:19:08 +0900

    cicd - 확인을 위한 테스트 및 cros 수정
    변경내용
    -SecurityConfig 의 요청 포트 변경

--- src/main/java/com/team/RecipeRadar/global/config/SecurityConfig.java
+++ src/main/java/com/team/RecipeRadar/global/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList("*"));       //모든 요청의 허용
         config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
         config.setAllowedHeaders(Arrays.asList("*"));
 
