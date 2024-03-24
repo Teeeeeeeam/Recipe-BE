70b566085a765b43aecd8a4968d1d26ac4904955
kkssbbb <03337@naver.com>
2024-03-24 23:16:56 +0900

    feat: querydsl 의존성 추가
    내용:
    레시피 조회시 조회 쿼리를 동적으로 변경하기 위해 querydsl의존성 추가 및 관련 레파지토리 코드 추가

--- src/main/java/com/team/RecipeRadar/domain/recipe/dao/RecipeRepository.java
+++ src/main/java/com/team/RecipeRadar/domain/recipe/dao/RecipeRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    List<Recipe> findByRecipeTitleContainingIgnoreCase(String recipeTitle);
+public interface RecipeRepository {
 
-    long countByRecipeTitleIsNotNull();
 }
