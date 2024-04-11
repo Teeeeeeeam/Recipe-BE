50a867623e18569b3d2aee1cfe473e09f44c4c07
CHISANW <keuye06380618@gmail.com>
2024-04-11 16:37:04 +0900

    test : 사용자 페이지 - 닉네임, 이메일변경 단위 테스트 작성
    테스트 작성내용
    - 이메일 변경과 닉네임 변경에 대한 server, controller 단위 테스트 작성및 성공

---
+++ src/test/java/com/team/RecipeRadar/domain/userInfo/application/UserInfoServiceImplTest.java
@@ -0,0 +1,177 @@
+package com.team.RecipeRadar.domain.userInfo.application;
+
+import com.team.RecipeRadar.domain.member.application.MemberServiceImpl;
+import com.team.RecipeRadar.domain.member.dao.MemberRepository;
+import com.team.RecipeRadar.domain.member.domain.Member;
+import com.team.RecipeRadar.domain.userInfo.dto.info.UserInfoResponse;
+import com.team.RecipeRadar.global.email.application.AccountRetrievalEmailServiceImpl;
+import com.team.RecipeRadar.global.exception.ex.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.access.AccessDeniedException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+class UserInfoServiceImplTest {
+
+
+    @Mock
+    MemberRepository memberRepository;
+
+    @Mock
+    MemberServiceImpl memberService;
+    @Mock
+    AccountRetrievalEmailServiceImpl accountRetrievalEmailService;
+
+    @InjectMocks
+    UserInfoServiceImpl userInfoService;
+
+    @Test
+    @DisplayName("사용자 페이지의 개인 정보를 불러오는 테스트")
+    void getMembers() {
+        // 가짜 토큰과 가짜 로그인 ID 설정
+        String loginId = "testId";
+        String username = "이름";
+
+        // 가짜 멤버 객체 생성
+        Member member = Member.builder()
+                .id(1L)
+                .loginId(loginId)
+                .username(username)
+                .nickName("닉네임")
+                .email("이메일")
+                .login_type("normal").build();
+
+        // 가짜 멤버 객체 반환 설정
+        when(memberRepository.findByLoginId(loginId)).thenReturn(member);
+
+        // getMembers 메서드 호출 및 반환된 UserInfoResponse 객체 저장
+        UserInfoResponse members = userInfoService.getMembers(loginId, username);
+
+        // 검증
+        assertThat(members.getUsername()).isEqualTo("이름");
+        assertThat(members.getNickName()).isEqualTo("닉네임");
+        assertThat(members.getEmail()).isEqualTo("이메일");
+        assertThat(members.getLoginType()).isEqualTo("normal");
+    }
+
+    @Test
+    @DisplayName("사용자 페이지의 다른 사용자 접근시 예외 발생 테스트")
+    void getMembers_AccessDenied() {
+        String loginId = "testId";
+        String differentUsername= "differentUsername";       //다른 사용자 ID
+
+
+        // 가짜 멤버 객체 생성
+        Member member = Member.builder()
+                .id(1L)
+                .loginId(loginId)
+                .username("이름")
+                .nickName("닉네임")
+                .email("이메일")
+                .login_type("normal").build();
+
+        // 가짜 멤버 객체 반환 설정
+        when(memberRepository.findByLoginId(loginId)).thenReturn(member);
+
+        // AccessDeniedException 예외 발생
+        assertThatThrownBy(() -> userInfoService.getMembers(loginId,differentUsername)).isInstanceOf(AccessDeniedException.class);
+
+    }
+
+    @Test
+    @DisplayName("사용자 페이지에서 닉네임을 변경하는 테스트")
+    void update_NickName_success(){
+        String loginId = "loginId";
+        String aFNickName = "afterName";
+        String authName = "username";
+
+
+        Member member = Member.builder().id(1l).loginId(loginId).username("username").nickName("before").build();
+
+        when(memberRepository.findByLoginId(loginId)).thenReturn(member);
+
+        userInfoService.updateNickName(aFNickName,loginId,authName);
+
+        assertThat(member.getNickName()).isEqualTo(aFNickName);
+    }
+
+    @Test
+    @DisplayName("사용자 페이지에서 닉네임을 변경중 실패하는 테스트")
+    void update_NickName_fail(){
+        String loginId = "loginId";
+        String aFNickName = "afterName";
+        String fakeAuthName = "fakeUsername";
+
+
+        Member member = Member.builder().id(1l).loginId(loginId).username("username").nickName("before").build();
+
+        when(memberRepository.findByLoginId(loginId)).thenReturn(member);
+
+        assertThatThrownBy(() -> userInfoService.updateNickName(aFNickName,loginId,fakeAuthName)).isInstanceOf(AccessDeniedException.class);
+    }
+
+    @Test
+    @DisplayName("사용자 페이지에서 이메일 변경 성공 테스트")
+    void update_email_success(){
+        String loginId = "testId";
+        String AfterEmail = "afEmail@email.com";
+        String autName = "username";
+        Member member = Member.builder().username("username").nickName("nickName").loginId(loginId).email("test@email.com").build();
+
+        when(memberRepository.findByLoginId(loginId)).thenReturn(member);
+
+        Map<String,Boolean> emailValidMap = new HashMap<>();
+        emailValidMap.put("duplicateEmail",true);
+        emailValidMap.put("useEmail",true);
+
+        when(memberService.emailValid(AfterEmail)).thenReturn(emailValidMap);
+
+        Map<String,Boolean> verifyCodeMap = new HashMap<>();
+        verifyCodeMap.put("isVerifyCode",true);
+
+        when(memberService.verifyCode(AfterEmail,123456)).thenReturn(verifyCodeMap);
+
+        userInfoService.updateEmail(AfterEmail,"123456",loginId,autName);
+
+        assertThat(member.getEmail()).isEqualTo(AfterEmail);
+    }
+
+    @Test
+    @DisplayName("사용자 페이지에서 이메일 변경주 이메일 관련 예외 테스트")
+    void update_email_fail(){
+        String loginId = "testId";
+        String AfterEmail = "afEmail@email.com";
+        String autName = "username";
+        Member member = Member.builder().username("username").nickName("nickName").loginId(loginId).email("test@email.com").build();
+
+        when(memberRepository.findByLoginId(loginId)).thenReturn(member);
+
+        Map<String,Boolean> emailValidMap = new HashMap<>();
+        emailValidMap.put("duplicateEmail",false);
+        emailValidMap.put("useEmail",true);
+
+        when(memberService.emailValid(AfterEmail)).thenReturn(emailValidMap);
+
+        Map<String,Boolean> verifyCodeMap = new HashMap<>();
+        verifyCodeMap.put("isVerifyCode",true);
+
+        when(memberService.verifyCode(AfterEmail,123456)).thenReturn(verifyCodeMap);
+
+        assertThatThrownBy(() -> userInfoService.updateEmail(AfterEmail,"123456",loginId,autName)).isInstanceOf(BadRequestException.class);
+    }
+
+
+}
