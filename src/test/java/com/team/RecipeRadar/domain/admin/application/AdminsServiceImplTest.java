7a42bbc2c7284841293f9fd50310a03a0e644d85
CHISANW <keuye06380618@gmail.com>
2024-05-22 15:59:16 +0900

    test : 어드민 사용자 조회 API 단위 테스트 작성

--- src/test/java/com/team/RecipeRadar/domain/admin/application/AdminsServiceImplTest.java
+++ src/test/java/com/team/RecipeRadar/domain/admin/application/AdminsServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.team.RecipeRadar.domain.admin.application;
 
+import com.team.RecipeRadar.domain.admin.dto.MemberInfoResponse;
 import com.team.RecipeRadar.domain.member.dao.MemberRepository;
+import com.team.RecipeRadar.domain.member.domain.Member;
 import com.team.RecipeRadar.domain.post.dao.PostRepository;
 import com.team.RecipeRadar.domain.recipe.dao.recipe.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -55,5 +63,26 @@ class AdminsServiceImplTest {
 
         long l = adminService.searchAllPosts();
         assertThat(l).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("전체 사용자 무한 페이징")
+    public void infinite_page(){
+        Pageable pageRequest = PageRequest.of(0, 2);
+        String loginId = "testId";
+
+        List<Member> memberList = List.of( Member.builder().username("회원1").email("email1").loginId(loginId).nickName("닉네임1").join_date(LocalDate.now()).build()
+                , Member.builder().username("회원2").email("email2").loginId("loginId2").nickName("닉네임2").join_date(LocalDate.now()).build());
+
+        boolean hasNext = true;
+
+        SliceImpl<Member> memberSlice = new SliceImpl<>(memberList, pageRequest, hasNext);
+        when(memberRepository.getMemberInfo(pageRequest)).thenReturn(memberSlice);
+
+        MemberInfoResponse memberInfoResponse = adminService.memberInfos(pageRequest);
+
+        assertThat(memberInfoResponse.getNextPage()).isTrue();
+        assertThat(memberInfoResponse.getMemberInfos()).hasSize(2);
+        assertThat(memberInfoResponse.getMemberInfos().get(0).getLoginId()).isEqualTo(loginId);
     }
 }
