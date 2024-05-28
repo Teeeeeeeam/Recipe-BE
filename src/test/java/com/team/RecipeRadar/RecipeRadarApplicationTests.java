7d8a5d864737db4cdf3d1d882834ac0a3e8d24d6
CHISANW <keuye06380618@gmail.com>
2024-05-28 16:14:25 +0900

    test : 기본 테스트 클래스 @SpingBootTest 주석처리

--- src/test/java/com/team/RecipeRadar/RecipeRadarApplicationTests.java
+++ src/test/java/com/team/RecipeRadar/RecipeRadarApplicationTests.java
@@ -3,7 +3,7 @@ package com.team.RecipeRadar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class RecipeRadarApplicationTests {
 
 	@Test
