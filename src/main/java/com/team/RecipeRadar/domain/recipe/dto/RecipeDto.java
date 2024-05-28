afa303dff2555c44358a007d203825d2255d8934
CHISANW <keuye06380618@gmail.com>
2024-05-28 16:12:50 +0900

    remove : RecipeDto의  of메소드(Recipe의 imeUrl포함된) 삭제

--- src/main/java/com/team/RecipeRadar/domain/recipe/dto/RecipeDto.java
+++ src/main/java/com/team/RecipeRadar/domain/recipe/dto/RecipeDto.java
@@ -53,9 +53,6 @@ public class RecipeDto {
     public static RecipeDto from(Long id, String imageUrl, String title, String cookingLevel, String people, String cookingTime, Integer likeCount){
         return new RecipeDto(id,imageUrl,title,cookingLevel,people,cookingTime,likeCount);
     }
-    public static RecipeDto of(Recipe recipe){
-        return new RecipeDto(recipe.getId(), recipe.getImageUrl(), recipe.getTitle(), recipe.getCookingLevel(), recipe.getPeople(), recipe.getCookingTime(), recipe.getLikeCount());
-    }
 
     public static RecipeDto of(Recipe recipe,String imageUrl,List<CookingStep> cookStep,String ingredient){
         return new RecipeDto(recipe.getId(), imageUrl, recipe.getTitle(), recipe.getCookingLevel(), recipe.getPeople(), recipe.getCookingTime(), recipe.getLikeCount()
