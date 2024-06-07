f551268cbd8346c76be788259ae6dc52392d55fb
CHISANW <keuye06380618@gmail.com>
2024-06-07 23:43:26 +0900

    refacotr : 레시피,게시글 페이징 responsedate content_id 추가

--- src/main/java/com/team/RecipeRadar/domain/like/dto/UserLikeDto.java
+++ src/main/java/com/team/RecipeRadar/domain/like/dto/UserLikeDto.java
@@ -9,25 +9,28 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserLikeDto {
 
-    private Long id;
+    private Long like_id;
+    private Long content_id;
     private String content;
     private String title;
 
-    public UserLikeDto(Long id, String content, String title) {
-        this.id = id;
+    public UserLikeDto(Long id,Long content_id, String content, String title) {
+        this.like_id = id;
+        this.content_id=content_id;
         this.content = content;
         this.title = title;
     }
 
-    public UserLikeDto(Long id, String title) {
-        this.id = id;
+    public UserLikeDto(Long id,Long content_id, String title) {
+        this.like_id = id;
+        this.content_id=content_id;
         this.title = title;
     }
 
     public static UserLikeDto Post_of(PostLike postLike){
-        return new UserLikeDto(postLike.getId(),postLike.getPost().getPostContent(),postLike.getPost().getPostTitle());
+        return new UserLikeDto(postLike.getId(),postLike.getPost().getId(),postLike.getPost().getPostContent(),postLike.getPost().getPostTitle());
     }
     public static UserLikeDto Recipe_of(RecipeLike recipeLike){
-        return new UserLikeDto(recipeLike.getId(),recipeLike.getRecipe().getTitle());
+        return new UserLikeDto(recipeLike.getId(),recipeLike.getRecipe().getId(),recipeLike.getRecipe().getTitle());
     }
 }
