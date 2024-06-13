64d8216b203333697c5805dd6621235f5cc77770
CHISANW <keuye06380618@gmail.com>
2024-06-14 05:08:39 +0900

    feat : 댓글 예외 타입 작성

--- src/main/java/com/team/RecipeRadar/global/exception/ex/NoSuchErrorType.java
+++ src/main/java/com/team/RecipeRadar/global/exception/ex/NoSuchErrorType.java
@@ -6,6 +6,7 @@ public enum NoSuchErrorType {
     NO_SUCH_POST("게시글을 찾을 수 없습니다."),
     NO_SUCH_RECIPE("레시피를 찾을 수 없습니다."),
     NO_SUCH_IMAGE("이미지 파일을 찾을 수 없습니다."),
+    NO_SUCH_COMMENT("해당 댓글을 찾을 수 없습니다."),
     NO_SUCH_COOK_STEP("조리 순서를 찾을수 없습니다.");
 
     private final String message;
