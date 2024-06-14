a958690d075fe2a1a37f3838b4b6d55e30b0bbf4
CHISANW <keuye06380618@gmail.com>
2024-06-14 22:32:18 +0900

    feat : 사용자 예외 추가(사용 할 수 없는 타입시)

---
+++ src/main/java/com/team/RecipeRadar/global/exception/ex/InvalidIdException.java
@@ -0,0 +1,8 @@
+package com.team.RecipeRadar.global.exception.ex;
+
+public class InvalidIdException extends RuntimeException{
+
+    public InvalidIdException(String message) {
+        super(message);
+    }
+}
