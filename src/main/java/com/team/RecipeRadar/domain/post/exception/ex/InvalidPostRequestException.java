79b69834fa4ee890f0cb03d92c6f8411414c048c
CHISANW <keuye06380618@gmail.com>
2024-06-17 20:00:43 +0900

    remove : post exception 디렉터리 삭제

--- src/main/java/com/team/RecipeRadar/domain/post/exception/ex/InvalidPostRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.team.RecipeRadar.domain.post.exception.ex;
-
-public class InvalidPostRequestException extends RuntimeException{
-    public InvalidPostRequestException (String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/team/RecipeRadar/domain/post/exception/ex/UnauthorizedPostException.java
+++
@@ -1,7 +0,0 @@
-package com.team.RecipeRadar.domain.post.exception.ex;
-
-public class UnauthorizedPostException extends RuntimeException {
-    public  UnauthorizedPostException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
