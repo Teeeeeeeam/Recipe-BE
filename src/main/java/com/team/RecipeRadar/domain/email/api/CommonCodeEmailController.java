7de03acbeca657461b73c3817f8917538d1280dd
CHISANW <keuye06380618@gmail.com>
2024-06-17 19:58:54 +0900

    refactor : 공용 인증코드 이메일 검증 API 리팩토링
    - /api/search/email-confirmation/verify -> /api/code/email-confirmation/verify 앤드포인트 수정
    - @RequestParam("email") 에서 @RequestBody EmailVerificationRequest 변경

--- src/main/java/com/team/RecipeRadar/domain/email/api/CommonCodeEmailController.java
+++ src/main/java/com/team/RecipeRadar/domain/email/api/CommonCodeEmailController.java
@@ -2,6 +2,7 @@ package com.team.RecipeRadar.domain.email.api;
 
 import com.team.RecipeRadar.domain.admin.domain.BlackListRepository;
 import com.team.RecipeRadar.domain.email.application.MailService;
+import com.team.RecipeRadar.domain.email.dto.EmailVerificationRequest;
 import com.team.RecipeRadar.global.exception.ErrorResponse;
 import com.team.RecipeRadar.global.payload.ControllerApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,16 +16,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
-
-@RestController
 @Tag(name = "공통 - 이메일 전송 컨트롤러",description = "이메일 인증 번호 전송 및 검증")
+@RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/api/code/email-confirmation")
 public class CommonCodeEmailController {
 
     @Qualifier("AccountEmail")
@@ -38,7 +42,7 @@ public class CommonCodeEmailController {
                     content = @Content(schema = @Schema(implementation = ControllerApiResponse.class),
                             examples = @ExampleObject(value = "{\"success\": true, \"message\": \"메일 전송 성공\"}")))
     })
-    @PostMapping("/api/code/email-confirmation/send")
+    @PostMapping("/send")
     public ResponseEntity<?> mailConfirm(@Parameter(description ="이메일") @RequestParam("email") String email){
         boolean existsByEmail = blackListRepository.existsByEmail(email);
         if(!existsByEmail) {
@@ -56,9 +60,25 @@ public class CommonCodeEmailController {
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class),
                             examples = @ExampleObject(value = "{\"success\": false, \"message\": \"[인증번호가 일치하지 않습니다. or 숫자만 입력해주세요.]\"}")))
     })
-    @PostMapping("/api/code/email-confirmation/verify")
-    public ResponseEntity<?> check(@RequestParam("email")String email, @RequestParam("code")String userCode){
-            Map<String, Boolean> stringBooleanMap = mailService.verifyCode(email,Integer.parseInt(userCode));
-            return ResponseEntity.ok(new ControllerApiResponse<>(true,"성공",stringBooleanMap));
+    @PostMapping("/verify")
+    public ResponseEntity<?> check(@Valid @RequestBody EmailVerificationRequest signUpEmailVerificationRequest, BindingResult bindingResult){
+        ResponseEntity<ErrorResponse<List<String>>> result = getErrorResponseResponseEntity(bindingResult);
+        if (result != null) return result;
+
+        Map<String, Boolean> stringBooleanMap = mailService.verifyCode(signUpEmailVerificationRequest.getEmail(), signUpEmailVerificationRequest.getCode());
+        if(!stringBooleanMap.get("isVerifyCode")) throw new IllegalStateException("인증번호가 일치하지 않습니다.");
+
+        return ResponseEntity.ok(new ControllerApiResponse<>(true,"성공",stringBooleanMap));
+    }
+
+    private static ResponseEntity<ErrorResponse<List<String>>> getErrorResponseResponseEntity(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> result = new LinkedList<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                result.add( error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(new ErrorResponse<>(false, "실패", result));
+        }
+        return null;
     }
 }
