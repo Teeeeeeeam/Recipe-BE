cdf1b3d17e32d70ee3541074305c72b9e8d75952
CHISANW <keuye06380618@gmail.com>
2024-06-18 15:11:05 +0900

    test : 방문자 API 리팩토링의 따른 테스트 코드 수정

--- src/test/java/com/team/RecipeRadar/domain/visit/api/VisitSessionControllerTest.java
+++ src/test/java/com/team/RecipeRadar/domain/visit/api/VisitSessionControllerTest.java
@@ -2,6 +2,7 @@ package com.team.RecipeRadar.domain.visit.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.RecipeRadar.domain.member.dao.MemberRepository;
+import com.team.RecipeRadar.domain.userInfo.utils.CookieUtils;
 import com.team.RecipeRadar.domain.visit.domain.VisitSession;
 import com.team.RecipeRadar.global.jwt.utils.JwtProvider;
 import com.team.RecipeRadar.global.security.oauth2.CustomOauth2Handler;
@@ -12,12 +13,15 @@ import com.team.RecipeRadar.domain.visit.dto.DayDto;
 import com.team.RecipeRadar.domain.visit.dto.MonthDto;
 import com.team.RecipeRadar.domain.visit.dto.WeekDto;
 import com.team.mock.CustomMockAdmin;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.http.Cookie;
@@ -26,54 +30,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.time.LocalDate.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(VisitCountController.class)
 class VisitSessionControllerTest {
 
-    @MockBean private VisitSessionRepository visitRepository;
-    @MockBean private VisitService visitService;
+    @MockBean VisitSessionRepository visitRepository;
+    @MockBean VisitService visitService;
+    @MockBean CookieUtils cookieUtils;
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Autowired private MockMvc mockMvc;
 
-    @MockBean
-    MemberRepository memberRepository;
-    @MockBean
-    JwtProvider jwtProvider;
-    @MockBean
-    CustomOauth2Handler customOauth2Handler;
-    @MockBean
-    CustomOauth2Service customOauth2Service;
-
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @MockBean MemberRepository memberRepository;
+    @MockBean JwtProvider jwtProvider;
+    @MockBean CustomOauth2Handler customOauth2Handler;
+    @MockBean CustomOauth2Service customOauth2Service;
 
     @Test
     @DisplayName("처음 방문할시에 쿠키 발급후 db에 저장")
     void ipCount() throws Exception {
-        given(visitRepository.existsByIpAddress(any())).willReturn(false);
+        // given
+        given(visitRepository.existsByIpAddress(anyString())).willReturn(false);
         given(visitRepository.save(any(VisitSession.class))).willReturn(new VisitSession());
 
-        Cookie cookie = new Cookie("visitors", "fakeCookie");
+        ResponseCookie responseCookie = ResponseCookie.from("visitors", "123.456.789.000").build();
+        given(cookieUtils.createCookie(anyString(),anyString(),anyInt())).willReturn(responseCookie);
+        // when & then
         mockMvc.perform(post("/api/visit")
-                        .cookie(cookie)
                         .header("X-Forwarded-For", "123.456.789.000")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.message").value("방문 성공"));
+                .andExpect(jsonPath("$.message").value("방문 성공"))
+                .andExpect(header().exists(HttpHeaders.SET_COOKIE))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE, CoreMatchers.containsString("visitors=")));
     }
+
 
     @Test
     @DisplayName("이미 방문한 사용자가 요청시 400 에러")
     void ipCount_Fails() throws Exception {
-        given(visitRepository.existsByIpAddress(any())).willReturn(true);
+        when(visitService.ipExists(anyString())).thenThrow(new IllegalStateException("현재 조회된 IP주소 입니다."));
 
         Cookie cookie = new Cookie("visitors", "fakeCookie");
         mockMvc.perform(post("/api/visit")
