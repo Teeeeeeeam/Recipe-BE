185a3d4b4997b9a7ac30c15a1e5ffbf4144dd99a
CHISANW <keuye06380618@gmail.com>
2024-06-21 21:55:11 +0900

    feat : 토큰 삭제 쿼리 작성
    - 토큰값을 통해 토큰 삭제 쿼리 작성

---
+++ src/main/java/com/team/RecipeRadar/global/auth/dao/RefreshTokenRepositoryCustom.java
@@ -0,0 +1,8 @@
+package com.team.RecipeRadar.global.auth.dao;
+
+import com.team.RecipeRadar.domain.member.dto.MemberDto;
+
+public interface RefreshTokenRepositoryCustom {
+
+    MemberDto existsByRefreshTokenAndMember(String refreshToken, String loginId);
+}
---
+++ src/main/java/com/team/RecipeRadar/global/auth/dao/RefreshTokenRepositoryCustomImpl.java
@@ -0,0 +1,35 @@
+package com.team.RecipeRadar.global.auth.dao;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.team.RecipeRadar.domain.member.domain.Member;
+import com.team.RecipeRadar.domain.member.dto.MemberDto;
+import com.team.RecipeRadar.global.exception.ex.nosuch.NoSuchDataException;
+import com.team.RecipeRadar.global.exception.ex.nosuch.NoSuchErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.team.RecipeRadar.domain.member.domain.QMember.member;
+import static com.team.RecipeRadar.global.auth.domain.QRefreshToken.*;
+
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenRepositoryCustomImpl implements RefreshTokenRepositoryCustom{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public MemberDto existsByRefreshTokenAndMember(String token, String loginId) {
+
+        List<Member> members = jpaQueryFactory
+                .select(refreshToken1.member)  // refreshToken1.member를 select 절에 포함
+                .from(refreshToken1)
+                .leftJoin(refreshToken1).on(refreshToken1.member.id.eq(member.id)).fetchJoin()
+                .where(refreshToken1.member.loginId.eq(loginId)
+                        .and(refreshToken1.refreshToken.eq(token)))
+                .fetch();
+        return  members.stream().map(member -> MemberDto.from(member)).findFirst().orElseThrow(() -> new NoSuchDataException(NoSuchErrorType.NO_SUCH_MEMBER));
+
+    }
+}
