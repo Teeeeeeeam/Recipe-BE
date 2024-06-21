ef827eb5ff847d1b5546679b613f84931e8b0348
CHISANW <keuye06380618@gmail.com>
2024-06-21 21:48:02 +0900

    refactor : RestControllerAdvice 리팩토링
    - 사용하지 않는 코드 삭제

--- src/main/java/com/team/RecipeRadar/global/exception/advice/ApiControllerAdvice.java
+++ src/main/java/com/team/RecipeRadar/global/exception/advice/ApiControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.team.RecipeRadar.global.exception.advice;
 
-
 import com.team.RecipeRadar.global.exception.ex.*;
 import com.team.RecipeRadar.global.exception.ex.nosuch.NoSuchDataException;
 import com.team.RecipeRadar.global.payload.ErrorResponse;
@@ -8,18 +7,15 @@ import com.team.RecipeRadar.global.exception.ex.img.ImageException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ServerErrorException;
 import org.springframework.web.server.UnsupportedMediaTypeStatusException;
 
-import java.util.NoSuchElementException;
 
 @Slf4j
 @RestControllerAdvice
 public class ApiControllerAdvice {
-
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> ServerError (ServerErrorException ex){
@@ -27,45 +23,27 @@ public class ApiControllerAdvice {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> AccessDenied(AccessDeniedException e){
-        ErrorResponse errorResponse = new ErrorResponse(false, e.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
-    }
-
     @ExceptionHandler(JwtTokenException.class)
     public ResponseEntity<ErrorResponse> TokenError(JwtTokenException e){
         ErrorResponse errorResponse = new ErrorResponse(false, e.getMessage());
-        log.error("errorMessage={}",e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
-
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> UnSupport(UnsupportedMediaTypeStatusException e){
         ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(response);
     }
-    
-    /* NoSuchElementException의 예외를 400예외 처리 */
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> noSuch_BadRequest(NoSuchElementException e){
-        log.error("Exception occurred:", e);
-        ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
 
     /* IllegalArgumentException 예외를 403예외 처리 사용자가 아닌 타인 사용시)*/
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> illegal_Forbidden(IllegalArgumentException e){
-        log.error("Exception occurred:", e);
         ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> illegalState_BadRequest(IllegalStateException e){
-        log.error("Exception occurred:", e);
         ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
@@ -73,7 +51,6 @@ public class ApiControllerAdvice {
     /* 숫자 아닐시 400 예외 */
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> number_BadRequest(NumberFormatException e){
-        log.error("Exception occurred:", e);
         ErrorResponse response = new ErrorResponse<>(false, "숫자만 입력해주세요.");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
@@ -81,28 +58,24 @@ public class ApiControllerAdvice {
     /* 이미지 관련 예외 */
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> Image_BadRequest(ImageException e){
-        log.error("Exception occurred:", e);
         ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> NoSuch_BadRequest(NoSuchDataException e){
-        log.error("Exception occurred:", e);
         ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> Invalid_BadRequest(InvalidIdException e){
-        log.error("Exception occurred:", e);
         ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> Unauthorized_Forbidden(UnauthorizedException e){
-        log.error("Exception occurred:", e);
         ErrorResponse response = new ErrorResponse<>(false, e.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
