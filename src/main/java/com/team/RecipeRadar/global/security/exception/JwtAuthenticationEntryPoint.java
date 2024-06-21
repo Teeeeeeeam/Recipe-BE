bf179c19299fdc9f2121752d2e54ed8f1ce80b10
CHISANW <keuye06380618@gmail.com>
2024-06-21 21:45:32 +0900

    feat : 시큐티리 공통 예외 설정
    - 기존 시큐리티의 401, 403예외를 커스텀해서 공통되게 예외가 나오도록 설정

---
+++ src/main/java/com/team/RecipeRadar/global/security/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.team.RecipeRadar.global.security.exception;
+
+import com.team.RecipeRadar.global.exception.ex.UnauthorizedException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        throw new UnauthorizedException("로그인이 필요합니다.");
+    }
+}
