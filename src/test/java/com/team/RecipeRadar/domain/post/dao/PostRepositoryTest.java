66d37692ef9f0658c705476e0189e8c81709c500
CHISANW <keuye06380618@gmail.com>
2024-06-27 16:50:43 +0900

    test : 게시글 Repository 단위테스트 리랙토링
    - 중복된 코드를 @BeforEach로 분리

--- src/test/java/com/team/RecipeRadar/domain/post/dao/PostRepositoryTest.java
+++ src/test/java/com/team/RecipeRadar/domain/post/dao/PostRepositoryTest.java
@@ -11,7 +11,7 @@ import com.team.RecipeRadar.domain.recipe.domain.Recipe;
 import com.team.RecipeRadar.domain.Image.dao.ImgRepository;
 import com.team.RecipeRadar.domain.Image.domain.UploadFile;
 import com.team.RecipeRadar.global.config.QueryDslConfig;
-import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,19 +22,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @EnableJpaAuditing
 @Import(QueryDslConfig.class)
-@Transactional
 @ActiveProfiles("test")
-@Slf4j
 class PostRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
@@ -43,38 +40,40 @@ class PostRepositoryTest {
     @Autowired ImgRepository imgRepository;
     @Autowired RecipeRepository recipeRepository;
 
+    Member member;
+    Member member1;
+    Recipe saveRecipe;
+    List<Post> posts;
+
+    @BeforeEach
+    void setUp() {
+        member = Member.builder().loginId("testId").nickName("닉네임 1").build();
+        member1 = Member.builder().nickName("닉네임 2").build();
+        memberRepository.save(member);
+        memberRepository.save(member1);
+
+        Recipe recipe = Recipe.builder().id(1L).title("testTitle").build();
+        saveRecipe = recipeRepository.save(recipe);
+
+        List<Post> postList = new ArrayList<>();
+        postList.add(Post.builder().postTitle("searchPost").postContent("컨텐트1").recipe(saveRecipe).postCookingLevel("coo").postLikeCount(120).member(member).postCookingTime("ti").build());
+        postList.add(Post.builder().postTitle("tit1").postContent("컨텐트2").recipe(saveRecipe).postCookingLevel("coo").postLikeCount(1000).member(member1).postCookingTime("ti").build());
+        postList.add(Post.builder().postTitle("tit2").postContent("컨텐트3").recipe(saveRecipe).postCookingLevel("coo").postLikeCount(150).member(member).postCookingTime("ti").build());
+        postList.add(Post.builder().postTitle("tit3").postContent("컨텐트4").recipe(saveRecipe).postCookingLevel("coo").postLikeCount(0).member(member).postCookingTime("ti").build());
+
+        posts = postRepository.saveAll(postList);
+
+        imgRepository.save(UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(0)).recipe(saveRecipe).build());
+        imgRepository.save(UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(1)).recipe(saveRecipe).build());
+        imgRepository.save(UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(2)).recipe(saveRecipe).build());
+        imgRepository.save(UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(3)).recipe(saveRecipe).build());
+    }
 
     @Test
     @DisplayName("무한 페이징 테스트 진행")
-    void pagingTest(){
+    void pagingTest() {
         Pageable request = PageRequest.of(0, 2);
-
-        List<Post> postList = new ArrayList<>();
-        Member member = Member.builder().nickName("닉네임 1").build();
-        Member member1 = Member.builder().nickName("닉네임 2").build();
-        Member save = memberRepository.save(member);
-        Member save1 = memberRepository.save(member1);
-
-        Recipe recipe = Recipe.builder().id(1l).title("testTitle").build();
-        Recipe saveRecipe = recipeRepository.save(recipe);
-
-        postList.add(Post.builder().postTitle("tit").postContent("컨텐트1").recipe(saveRecipe).postCookingLevel("coo").member(save).postCookingTime("ti").build());
-        postList.add(Post.builder().postTitle("tit").postContent("컨텐트2").recipe(saveRecipe).postCookingLevel("coo").member(save1).postCookingTime("ti").build());
-        postList.add(Post.builder().postTitle("tit").postContent("컨텐트3").recipe(saveRecipe).postCookingLevel("coo").member(save).postCookingTime("ti").build());
-        postList.add(Post.builder().postTitle("tit").postContent("컨텐트4").recipe(saveRecipe).postCookingLevel("coo").member(save).postCookingTime("ti").build());
-
-        List<Post> posts = postRepository.saveAll(postList);
-
-        UploadFile uploadFile = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(0)).recipe(saveRecipe).build();
-        UploadFile uploadFile1 = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(1)).recipe(saveRecipe).build();
-        UploadFile uploadFile2 = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(2)).recipe(saveRecipe).build();
-        UploadFile uploadFile3 = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(3)).recipe(saveRecipe).build();
-        imgRepository.save(uploadFile);
-        imgRepository.save(uploadFile1);
-        imgRepository.save(uploadFile2);
-        imgRepository.save(uploadFile3);
-
-        Slice<PostDto> allPost = postRepository.getAllPost(null,request);
+        Slice<PostDto> allPost = postRepository.getAllPost(null, request);
 
         assertThat(allPost.hasNext()).isTrue();
         assertThat(allPost.getContent()).hasSize(2);
@@ -82,58 +81,25 @@ class PostRepositoryTest {
 
     @Test
     @DisplayName("게시글 상세 조회 테스트")
-    void details_Posts(){
-        Member member = Member.builder().nickName("닉네임 1").build();
-        Member save = memberRepository.save(member);
-        Recipe recipe = Recipe.builder().id(1l).title("testTitle").build();
-        Recipe saveRecipe = recipeRepository.save(recipe);
-        Post post = Post.builder().postCookingTime("시간").postServing("인원수").postTitle("제목").postCookingLevel("난이도").member(save).recipe(saveRecipe).build();
+    void details_Posts() {
+        Comment comment = Comment.builder().post(posts.get(0)).commentContent("댓글 ㅈ가성~").member(member).build();
+        commentRepository.save(comment);
 
-        Comment comment = Comment.builder().post(post).commentContent("댓글 ㅈ가성~").member(member).build();
-        Comment save1 = commentRepository.save(comment);
+        PostDto postDto = postRepository.postDetails(posts.get(0).getId());
 
-        UploadFile uploadFile = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(post).recipe(saveRecipe).build();
-        imgRepository.save(uploadFile);
-
-        postRepository.save(post);
-        PostDto postDto = postRepository.postDetails(post.getId());
         assertThat(postDto).isNotNull();
-        assertThat(postDto.getPostTitle()).isEqualTo(post.getPostTitle());
-        assertThat(postDto.getComments().get(0).getCommentContent()).isEqualTo(save1.getCommentContent());
+        assertThat(postDto.getPostTitle()).isEqualTo(posts.get(0).getPostTitle());
+        assertThat(postDto.getComments().get(0).getCommentContent()).isEqualTo(comment.getCommentContent());
     }
 
     @Test
     @DisplayName("게시글 검색 페이징 테스트 진행")
-    void post_search(){
+    void post_search() {
         Pageable request = PageRequest.of(0, 2);
         String loginId = "testId";
-        List<Post> postList = new ArrayList<>();
-        Member member = Member.builder().loginId(loginId).nickName("닉네임 1").build();
-        Member member1 = Member.builder().loginId("nosearch").nickName("닉네임 2").build();
-        Member save = memberRepository.save(member);
-        Member save1 = memberRepository.save(member1);
 
-        Recipe recipe = Recipe.builder().id(1l).title("testTitle").build();
-        Recipe saveRecipe = recipeRepository.save(recipe);
-
-        postList.add(Post.builder().postTitle("searchPost").postContent("컨텐트1").recipe(saveRecipe).postCookingLevel("coo").member(save).postCookingTime("ti").build());
-        postList.add(Post.builder().postTitle("tit").postContent("컨텐트2").recipe(saveRecipe).postCookingLevel("coo").member(save1).postCookingTime("ti").build());
-        postList.add(Post.builder().postTitle("tit").postContent("컨텐트3").recipe(saveRecipe).postCookingLevel("coo").member(save).postCookingTime("ti").build());
-        postList.add(Post.builder().postTitle("tit").postContent("컨텐트4").recipe(saveRecipe).postCookingLevel("coo").member(save).postCookingTime("ti").build());
-
-        List<Post> posts = postRepository.saveAll(postList);
-
-        UploadFile uploadFile = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(0)).recipe(saveRecipe).build();
-        UploadFile uploadFile1 = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(1)).recipe(saveRecipe).build();
-        UploadFile uploadFile2 = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(2)).recipe(saveRecipe).build();
-        UploadFile uploadFile3 = UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(3)).recipe(saveRecipe).build();
-        imgRepository.save(uploadFile);
-        imgRepository.save(uploadFile1);
-        imgRepository.save(uploadFile2);
-        imgRepository.save(uploadFile3);
-
-        Slice<PostDto> search_loginId = postRepository.searchPosts(loginId,null,null,null,request);
-        Slice<PostDto> search_loginId_and_title_postTitle = postRepository.searchPosts(loginId,"testTitle","searchPost",null,request);
+        Slice<PostDto> search_loginId = postRepository.searchPosts(loginId, null, null, null, request);
+        Slice<PostDto> search_loginId_and_title_postTitle = postRepository.searchPosts(null, null, "searchPost", null, request);
 
         assertThat(search_loginId.hasNext()).isTrue();
         assertThat(search_loginId.getContent()).hasSize(2);
@@ -141,6 +107,5 @@ class PostRepositoryTest {
         assertThat(search_loginId_and_title_postTitle.hasNext()).isFalse();
         assertThat(search_loginId_and_title_postTitle.getContent()).hasSize(1);
     }
-
 
 }
