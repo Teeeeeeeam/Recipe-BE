2244e2ce96e5be86ac8b00e2788b62e760347a68
CHISANW <keuye06380618@gmail.com>
2024-06-29 06:20:10 +0900

    rename : 삭제 변수명 변경

--- src/main/java/com/team/RecipeRadar/domain/like/dao/like/PostLikeRepository.java
+++ src/main/java/com/team/RecipeRadar/domain/like/dao/like/PostLikeRepository.java
@@ -2,9 +2,6 @@ package com.team.RecipeRadar.domain.like.dao.like;
 
 import com.team.RecipeRadar.domain.like.domain.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,7 +9,5 @@ public interface PostLikeRepository extends JpaRepository<PostLike,Long>, PostLi
 
     Boolean existsByMemberIdAndPostId(Long memberId, Long postId);
     void deleteByMemberIdAndPostId(Long memberId, Long postId);
-    @Modifying
-    @Query("DELETE from PostLike p where p.post.id =:postId")
-    void deletePostID(@Param("postId")Long postId);
+    void deleteByPostId(Long postId);
 }
