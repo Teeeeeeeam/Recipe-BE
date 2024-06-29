d62c108ff01a87c5c0b180b3862b62d7bd461f0d
CHISANW <keuye06380618@gmail.com>
2024-06-29 22:10:07 +0900

    remove : @Slf4j 삭제

--- src/main/java/com/team/RecipeRadar/domain/visit/application/VisitServiceImpl.java
+++ src/main/java/com/team/RecipeRadar/domain/visit/application/VisitServiceImpl.java
@@ -7,14 +7,12 @@ import com.team.RecipeRadar.domain.visit.dto.DayDto;
 import com.team.RecipeRadar.domain.visit.dto.MonthDto;
 import com.team.RecipeRadar.domain.visit.dto.WeekDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
