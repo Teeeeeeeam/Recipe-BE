678f293e4bf4220f69594d36613c4ae9f33c4943
CHISANW <keuye06380618@gmail.com>
2024-07-04 21:20:07 +0900

    test : 게시글 검색 단위 테스트 수정
    - 네이티브 함수를 사용하기떄문에 order을 순서를 부여해 이전 테스트시 rollback fasle로 데이터를 남긴후 게시글 검색 테스트가 끝나면 해당 데이터를 삭제하도록 하도록 수정

--- src/test/java/com/team/RecipeRadar/domain/post/dao/PostRepositoryTest.java
+++ src/test/java/com/team/RecipeRadar/domain/post/dao/PostRepositoryTest.java
@@ -11,16 +11,17 @@ import com.team.RecipeRadar.domain.recipe.domain.Recipe;
 import com.team.RecipeRadar.domain.Image.dao.ImgRepository;
 import com.team.RecipeRadar.domain.Image.domain.UploadFile;
 import com.team.RecipeRadar.global.config.QueryDslConfig;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
@@ -29,8 +30,11 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Slf4j
 @EnableJpaAuditing
 @Import(QueryDslConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ActiveProfiles("test")
 class PostRepositoryTest {
 
@@ -40,10 +44,10 @@ class PostRepositoryTest {
     @Autowired ImgRepository imgRepository;
     @Autowired RecipeRepository recipeRepository;
 
-    Member member;
-    Member member1;
-    Recipe saveRecipe;
-    List<Post> posts;
+    private Member member;
+    private Member member1;
+    private Recipe saveRecipe;
+    private List<Post> posts;
 
     @BeforeEach
     void setUp() {
@@ -68,7 +72,7 @@ class PostRepositoryTest {
         imgRepository.save(UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(2)).recipe(saveRecipe).build());
         imgRepository.save(UploadFile.builder().storeFileName("tesNmae").originFileName("originName").post(posts.get(3)).recipe(saveRecipe).build());
     }
-
+    @Order(1)
     @Test
     @DisplayName("무한 페이징 테스트 진행")
     void pagingTest() {
@@ -79,7 +83,9 @@ class PostRepositoryTest {
         assertThat(allPost.getContent()).hasSize(2);
     }
 
+    @Order(2)
     @Test
+    @Rollback(value = false)
     @DisplayName("게시글 상세 조회 테스트")
     void details_Posts() {
         Comment comment = Comment.builder().post(posts.get(0)).commentContent("댓글 ㅈ가성~").member(member).build();
@@ -92,14 +98,17 @@ class PostRepositoryTest {
         assertThat(postDto.getComments().get(0).getCommentContent()).isEqualTo(comment.getCommentContent());
     }
 
+    @Order(3)
     @Test
     @DisplayName("게시글 검색 페이징 테스트 진행")
     void post_search() {
+        Long id = recipeRepository.findAll().get(0).getId();
+        log.info("현재 모든 게시글 ={}",id);
         Pageable request = PageRequest.of(0, 2);
         String loginId = "testId";
 
         Slice<PostDto> search_loginId = postRepository.searchPosts(loginId, null, null, null, request);
-        Slice<PostDto> search_loginId_and_title_postTitle = postRepository.searchPosts(null, null, "searchPost", null, request);
+        Slice<PostDto> search_loginId_and_title_postTitle = postRepository.searchPosts(null, null, "search", null, request);
 
         assertThat(search_loginId.hasNext()).isTrue();
         assertThat(search_loginId.getContent()).hasSize(2);
@@ -108,6 +117,7 @@ class PostRepositoryTest {
         assertThat(search_loginId_and_title_postTitle.getContent()).hasSize(1);
     }
 
+    @Order(4)
     @Test
     @DisplayName("레시피 페이지에서 좋아요순 4개 출력")
     void getRecipeTopByLikes(){
@@ -119,6 +129,19 @@ class PostRepositoryTest {
         assertThat(topRecipesByLikes.get(3).getPostTitle()).isEqualTo("tit3");
     }
 
+    @Order(5)
+    @Test
+    @DisplayName("데이터 삭제")
+    @Rollback(value = false)
+    void deleteAll(){
+        commentRepository.deleteAll();
+        recipeRepository.deleteAll();
+        memberRepository.deleteAll();
+        imgRepository.deleteAll();
+        postRepository.deleteAll();
+    }
+
+    @Order(6)
     @Test
     @DisplayName("메인 페이지의 게시글 전체 게시글 3개 조회")
     void getMainTopByLikes(){
@@ -129,5 +152,6 @@ class PostRepositoryTest {
         assertThat(topMainByLikes.get(1).getPostTitle()).isEqualTo("tit2");
         assertThat(topMainByLikes.get(2).getPostTitle()).isEqualTo("searchPost");
     }
+
 
 }
