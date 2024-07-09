3d178ce2b454db0733e4c4f7f1f3baa75e0b0f46
CHISANW <keuye06380618@gmail.com>
2024-07-09 17:32:01 +0900

    docs : 어드민 레시피 Swagger 수정

--- src/main/java/com/team/RecipeRadar/domain/recipe/api/admin/AdminRecipeController.java
+++ src/main/java/com/team/RecipeRadar/domain/recipe/api/admin/AdminRecipeController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.util.*;
 
+@Tag(name = "어드민 - 레시피 컨트롤러", description = "레시피 관리")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
@@ -28,11 +30,11 @@ public class AdminRecipeController {
 
     private final AdminRecipeService adminService;
 
-    @Operation(summary = "레시피수 조회", description = "작성된 레시피의 수를 조회하는 API",tags = "어드민 - 레시피 컨트롤러")
+    @Operation(summary = "레시피수 조회", description = "작성된 레시피의 수를 조회하는 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK",
                     content = @Content(schema = @Schema(implementation = ControllerApiResponse.class),
-                            examples = @ExampleObject(value = "{\"success\": true, \"message\":\"조회 성공\", \"data\":\"10\"}"))),
+                            examples = @ExampleObject(value = "{\"success\": true, \"message\":\"조회 성공\", \"data\":10}"))),
     })
     @GetMapping("/recipes/count")
     public ResponseEntity<?> getAllRecipesCount(){
@@ -63,7 +65,7 @@ public class AdminRecipeController {
                             examples = @ExampleObject(value = "{\"success\":true,\"message\":\"레시피 등록 성공\"}"))),
             @ApiResponse(responseCode = "400",description = "BAD REQUEST",
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class),
-                            examples = @ExampleObject(value = "[{\"success\":false,\"message\":\"[\"대표사진을 등록해주세요\" OR \"이미지 파일만 등록 해주세요\" OR \"70MB 이하로 등록해주세요\"]\"},{\"success\":false,\"message\":\"실패\",\"data\":{\"필드명\" : \"필드 오류 내용\"}}]")))
+                            examples = @ExampleObject(value = "[{\"success\":false,\"message\":\"[대표사진을 등록해주세요 OR 이미지 파일만 등록 해주세요 OR 70MB 이하로 등록해주세요]\"},{\"success\":false,\"message\":\"실패\",\"data\":{\"필드명\" : \"필드 오류 내용\"}}]")))
     })
     @PostMapping(value = "/save/recipes", consumes= MediaType.MULTIPART_FORM_DATA_VALUE ,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> recipe_save(@Valid @RequestPart RecipeSaveRequest recipeSaveRequest, BindingResult bindingResult,
